ad76d57e0a45440457cc805cd03573e7d531e0ff
Mohamed Ahmed (Jimmy) <mohameahmed@microsoft.com>
2024-09-16 17:37:22 +0300

    update

--- scripts/bookmark_preprocessor.py
+++ scripts/bookmark_preprocessor.py
@@ -1,22 +1,24 @@
 import json
 
 # Open and read the JSON file
-with open("test_data/bookmark.json", "r") as file:
+file_name = "bookmark"
+with open(f"test_data/{file_name}.json", "r") as file:
     data = json.load(file)
 
 result = []
 
 def fetch_bookmark(obj, path):
     if "url" in obj:
-        result.append(f"{path};URL:{obj["url"]}")
+        result.append(f"{path}:<Title>{obj["title"]}</Title>:<URL>{obj["url"]}</URL>")
         return
-    path_arr = list(filter(str.strip, path.split(",")))
+    path_arr = list(filter(str.strip, path.split(":")))
     path_len = len(path_arr) if path_arr else 0
-    new_path = (f"RootFolder:{obj["title"]}" if path_len == 0 else f"{path};SubFolder{path_len}:{obj["title"]}") if len(obj["title"]) > 0 else path
+    new_path = (f"<RootFolder>{obj["title"]}</RootFolder>" if path_len == 0 else f"{path}:<SubFolder{path_len}>{obj["title"]}</SubFolder{path_len}>") if len(obj["title"]) > 0 else path
     for folder in obj['children']:
         fetch_bookmark(folder, new_path)
 
 
 fetch_bookmark(data[0],"")
 
-print(result)
+with open(f'test_data/{file_name}_ouptut.txt', 'w') as f:
+    json.dump(result, f)
