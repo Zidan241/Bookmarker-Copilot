e41bdc78b7089026b3515116ecf20d7af6218a4c
Mohamed Ahmed (Jimmy) <mohameahmed@microsoft.com>
2024-09-17 16:47:44 +0300

    update script

--- scripts/bookmark_preprocessor.py
+++ scripts/bookmark_preprocessor.py
@@ -1,15 +1,16 @@
 import json
+import uuid
 
 # Open and read the JSON file
-file_name = "bookmark"
-with open(f"test_data/{file_name}.json", "r") as file:
+file_name = "jimmy"
+with open(f"test_data/bookmark_input_{file_name}.json", "r") as file:
     data = json.load(file)
 
 result = []
 
 def fetch_bookmark(obj, path):
     if "url" in obj:
-        result.append(f"{path}:<Title>{obj["title"]}</Title>:<URL>{obj["url"]}</URL>")
+        result.append(f"{path}:<Id>{uuid.uuid4()}</Id>:<Title>{obj["title"]}</Title>:<URL>{obj["url"]}</URL>")
         return
     path_arr = list(filter(str.strip, path.split(":")))
     path_len = len(path_arr) if path_arr else 0
@@ -20,5 +21,5 @@ def fetch_bookmark(obj, path):
 
 fetch_bookmark(data[0],"")
 
-with open(f'test_data/{file_name}_ouptut.txt', 'w') as f:
+with open(f'test_data/bookmark_output_{file_name}.json', 'w') as f:
     json.dump(result, f)
