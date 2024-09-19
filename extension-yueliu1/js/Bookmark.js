4670f9161a32165320eb6a053e89043d69615e7f
Yue Liu <yueliu1@microsoft.com>
2024-09-20 02:56:49 +0800

    add folder and expand parent

--- extension-yueliu1/js/Bookmark.js
+++ extension-yueliu1/js/Bookmark.js
@@ -1,80 +1,89 @@
 export default class Bookmark {
   constructor() {
-    document.addEventListener('DOMContentLoaded', function() {
-      function createTree(node, parentElement) {
-        let li = document.createElement("li");
-        // Check if node is a folder or a bookmark
-        if (node.children) {
-          li.style.paddingLeft = 16 + "px";
-          li.classList.add("folder");
-          //li.className = 'list-item-folder';
-          li.innerHTML = `
-              <div class="list-container">
-                  <img class="icon-20px" alt="Toggle" src="img/Chevron.svg">
-                  <div class="item">
-                      <div class="favicon-wrapper">
-                          <img class="folder-icon" alt="Folder" src="img/folder_icon.png">
-                      </div>
-                      <div class="align-text">
-                          <div class="label">${node.title}</div>
-                      </div>
-                  </div>
-              </div>
-          `;
-
-          // Add click event to toggle folder
-          let listContainer = li.querySelector('.list-container');
-          let chevron = li.querySelector('.icon-20px');
-          let isExpanded = false;
-          listContainer.addEventListener('click', function () {
-              isExpanded = !isExpanded;
-              li.querySelector('ul').style.display = isExpanded ? 'block' : 'none';
-              chevron.classList.toggle('down', isExpanded);
-          });
-
-          // Create the nested UL element with padding for indentation
-          let ul = document.createElement("ul");
-          ul.style.display = "none"; // Initially hidden
-          li.appendChild(ul);
-
-          // Recursively build the tree for child nodes
-          node.children.forEach((child) => createTree(child, ul));
-        } else {
-          li.style.paddingLeft = 40 + "px";
-          li.classList.add("bookmark");
-
-          // Create favicon for bookmark
-          let faviconUrl = `https://www.google.com/s2/favicons?domain=${new URL(node.url).hostname}`;
-          //TODO: Fix can't load edge://favicon2 resource issue
-          //let faviconUrl = `chrome://favicon/${node.url}`;
-          li.innerHTML = `
-              <div class="list-container">
-                  <img class="icon-20px" alt="Favicon" src="${faviconUrl}">
-                  <div class="text2">
-                      <a class="list-item-text" href="${node.url}" title="${node.title}" target="_blank" rel="noreferrer">${node.title}</a>
-                  </div>
-              </div>
-          `;
-          let listContainer = li.querySelector('.list-container');
-          let link = li.querySelector('a');
-          listContainer.addEventListener('click', () => {
-            link.click();
-          });
-        }
-
-        parentElement.appendChild(li);
-      }
+    document.addEventListener('DOMContentLoaded', () => {
       // Fetch the bookmarks and build the tree
       chrome.bookmarks.getTree(bookmarkTreeNodes => {
         let treeContainer = document.getElementById("bookmarkTree");
         // Only process the children of the root node (Bookmark Bar, Other Bookmarks, Mobile Bookmarks)
         let rootChildren = bookmarkTreeNodes[0].children;
         rootChildren.forEach((rootChild) => {
-          createTree(rootChild, treeContainer);
+          this.createTree(rootChild, treeContainer);
         });
       });
     });
-    this.initI18n()
+    this.initI18n();
+  }
+
+  createTree(node, parentElement) {
+    let li = document.createElement("li");
+    // Check if node is a folder or a bookmark
+    if (node.children) {
+      this.InitFolderElement(li, node);
+      // Create the nested UL element with padding for indentation
+      let ul = document.createElement("ul");
+      ul.style.display = "none"; // Initially hidden
+      li.appendChild(ul);
+
+      // Recursively build the tree for child nodes
+      node.children.forEach((child) => this.createTree(child, ul));
+    } else {
+      this.initUrlElement(li, node);
+    }
+    parentElement.appendChild(li);
+  }
+
+  InitFolderElement(li, node) {
+    li.classList.add("folder");
+    li.id = node.id;
+    li.style.paddingLeft = 16 + "px";
+    //li.className = 'list-item-folder';
+    li.innerHTML = `
+        <div class="list-container">
+            <img class="icon-20px" alt="Toggle" src="img/Chevron.svg">
+            <div class="item">
+                <div class="favicon-wrapper">
+                    <img class="folder-icon" alt="Folder" src="img/folder_icon.png">
+                </div>
+                <div class="align-text">
+                    <div class="label">${node.title}</div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    // Add click event to toggle folder
+    let listContainer = li.querySelector('.list-container');
+    let chevron = li.querySelector('.icon-20px');
+    let isExpanded = false;
+    listContainer.addEventListener('click', function () {
+        isExpanded = !isExpanded;
+        li.querySelector('ul').style.display = isExpanded ? 'block' : 'none';
+        chevron.classList.toggle('down', isExpanded);
+    });
+  }
+
+  initUrlElement(li, node) {
+    li.id = node.id;
+    li.style.paddingLeft = 40 + "px";
+    li.classList.add("bookmark");
+
+    // Create favicon for bookmark
+    let faviconUrl = `https://www.google.com/s2/favicons?domain=${new URL(node.url).hostname}`;
+    //TODO: Fix can't load edge://favicon2 resource issue
+    //let faviconUrl = `chrome://favicon/${node.url}`;
+    li.innerHTML = `
+        <div class="list-container">
+            <img class="icon-20px" alt="Favicon" src="${faviconUrl}">
+            <div class="text2">
+                <a class="list-item-text" href="${node.url}" title="${node.title}" target="_blank" rel="noreferrer">${node.title}</a>
+            </div>
+        </div>
+    `;
+    let listContainer = li.querySelector('.list-container');
+    let link = li.querySelector('a');
+    listContainer.addEventListener('click', () => {
+      link.click();
+    });
   }
 
   initI18n() {
--- extension-yueliu1/js/index.js
+++ extension-yueliu1/js/index.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
   });
 });
 
-let myBookmark = new Bookmark();
+let myBookmarkList = new Bookmark();
 addCurrentTab();
 
 function addCurrentTab() {
@@ -38,8 +38,9 @@ function addCurrentTab() {
 
       chrome.bookmarks.search({ title: folderName }, (results) => {
           if (results.length === 0) {
-              chrome.bookmarks.create({ title: folderName }, (folder) => {
-                  addBookmarkToFolder(folder.id, activeTab);
+              chrome.bookmarks.create({ title: folderName , parentId:'1'/*set default root folder as bookmar bar*/}, (folder) => {
+                createHightlightFolder(folder);
+                addBookmarkToFolder(folder.id, activeTab);
               });
           } else {
               addBookmarkToFolder(results[0].id, activeTab);
@@ -47,18 +48,49 @@ function addCurrentTab() {
       });
   });
 }
+
+function createHightlightFolder(node){
+  let li = document.createElement("li");
+  //li.classList.add("highlight-folder");
+  myBookmarkList.InitFolderElement(li, node);
+  // Create the nested UL element with padding for indentation
+  let ul = document.createElement("ul");
+  ul.style.display = "none"; // Initially hidden
+  li.appendChild(ul);
+
+
+  // Recursively expand the tree by parent_id
+  openParentFolder(node.parentId);
+
+  let parent_element = document.getElementById(node.parentId);
+  parent_element.appendChild(li);
+}
+
+function openParentFolder(parent_id){
+  if(parent_id != '0'){
+    let parent_li = document.getElementById(parent_id);
+    parent_li.querySelector('ul').style.display = 'block';
+    let chevron = parent_li.querySelector('.icon-20px');
+    chevron.classList.toggle('down', true);
+    chrome.bookmarks.get(parent_id, (parentNode) => {
+      openParentFolder(parentNode.parentId);
+    });
+  }
+}
+
 function addBookmarkToFolder(folderId, activeTab) {
     chrome.bookmarks.create({
         parentId: folderId,
         title: activeTab.title,
         url: activeTab.url
     }, (newBookmark) => {
-        highlightAndScrollToFolder(folderId);
+      ScrollAndhighlight(folderId);
     });
 }
 
-function highlightAndScrollToFolder(folderId) {
-  chrome.bookmarks.getSubTree(folderId, (nodes) => {
+function ScrollAndhighlight(folderId) {
+//  let li = document.createElement("li");
+/*  chrome.bookmarks.getSubTree(folderId, (nodes) => {
       if (nodes.length > 0) {
           const folderNode = nodes[0];
           chrome.bookmarks.getChildren(folderNode.id, (children) => {
@@ -66,5 +98,5 @@ function highlightAndScrollToFolder(folderId) {
               alert(`Bookmark added to folder "${folderNode.title}" and folder expanded!`);
           });
       }
-  });
+  });*/
 }
