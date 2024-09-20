0f8b5eb9dec4e296634334a1639ba725381eb224
Mohamed Ahmed (Jimmy) <mohameahmed@microsoft.com>
2024-09-21 01:28:53 +0300

    update

--- scripts/bookmark_preprocessor.py
+++ scripts/bookmark_preprocessor.py
@@ -3,7 +3,7 @@ import uuid
 from urllib.parse import urlparse
 
 # Open and read the JSON file
-file_name = "jimmy"
+file_name = "example"
 with open(f"test_data/bookmark_input_{file_name}.json", "r") as file:
     data = json.load(file)
 
@@ -11,21 +11,18 @@ result = []
 
 def fetch_bookmark(obj, path):
     if "url" in obj:
-        parsed_url = urlparse(obj["url"])
-        domain = parsed_url.netloc
-        # result.append(f"{path}:<Id>{uuid.uuid4()}</Id>:<Title>{obj["title"]}</Title>:<Domain>{domain}</Domain>")
-        # result.append(f"<Id>{uuid.uuid4()}</Id>:<Title>{obj["title"]}</Title>:<Domain>{domain}</Domain>")
-        result.append([path,domain])
+        # parsed_url = urlparse(obj["url"])
+        # domain = parsed_url.netloc
+        # <Id>{uuid.uuid4()}</Id>
+        result.append(f"{path}:<Title>{obj["title"]}</Title>:<Domain>{obj["url"]}</Domain>")
         return
-    #path_arr = list(filter(str.strip, path.split(":")))
-    #path_len = len(path_arr) if path_arr else 0
-    #new_path = (f"<RootFolder>{obj["title"]}</RootFolder>" if path_len == 0 else f"{path}:<SubFolder{path_len}>{obj["title"]}</SubFolder{path_len}>") if len(obj["title"]) > 0 else path
+    path_arr = list(filter(str.strip, path.split(":")))
+    path_len = len(path_arr) if path_arr else 0
+    new_path = (f"<RootFolder>{obj["title"]}</RootFolder>" if path_len == 0 else f"{path}:<SubFolder{path_len}>{obj["title"]}</SubFolder{path_len}>") if len(obj["title"]) > 0 else path
     for folder in obj['children']:
-        fetch_bookmark(folder, obj["title"])
-
+        fetch_bookmark(folder, new_path)
 
 fetch_bookmark(data[0],"")
 
-output_name = "_folder_name"
-with open(f'test_data/bookmark_output_{file_name}{output_name}.json', 'w') as f:
+with open(f'test_data/bookmark_output_{file_name}.json', 'w') as f:
     json.dump(result, f)
